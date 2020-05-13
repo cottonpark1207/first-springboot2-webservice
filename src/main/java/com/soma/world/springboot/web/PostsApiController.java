86f4e9e57b726bc90033477708d15ab793afd98f
somin <cottonpark1207@gmail.com>
2020-05-13 22:29:04 +0900

    Fix the test issue(Json parsing issue).
    Apply API about save/update/findId. (Chapter 3.4)

--- src/main/java/com/soma/world/springboot/web/PostsApiController.java
+++ src/main/java/com/soma/world/springboot/web/PostsApiController.java
@@ -1,13 +1,11 @@
 package com.soma.world.springboot.web;
 
 import com.soma.world.springboot.service.posts.PostsService;
+import com.soma.world.springboot.web.dto.PostsResponseDto;
 import com.soma.world.springboot.web.dto.PostsSaveRequestDto;
 import com.soma.world.springboot.web.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RequiredArgsConstructor
 @RestController
@@ -19,8 +17,13 @@ public class PostsApiController {
         return postsService.save(requestDto);
     }
 
-    @PostMapping("/api/v1/posts/{id}")
+    @PutMapping("/api/v1/posts/{id}")
     public Long update(@PathVariable Long id, @RequestBody PostsUpdateRequestDto requestDto) {
         return postsService.update(id, requestDto);
+    }
+
+    @GetMapping("/api/v1/posts/{id}")
+    public PostsResponseDto findById (@PathVariable Long id) {
+        return postsService.findById(id);
     }
 }
